9e33990f6b2ef485e46d2f11d2b85efa7ff4cc31
antho <anthonyyoussef01@gmail.com>
2024-02-27 21:44:50 -0500

    Create board component

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-square',
+  template: `
+    <p>{{ value }}</p>
+  `,
   standalone: true,
-  imports: [],
-  templateUrl: './square.component.html',
   styleUrl: './square.component.css'
 })
 export class SquareComponent {
-
+  @Input() value: 'X' | 'O' | undefined;
 }
